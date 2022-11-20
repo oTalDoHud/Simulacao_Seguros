dbe752b29044d0c7c20fee32701d2852858ad985
oTalDoHud <hudson.lucas.vieira@gmail.com>
2022-11-19 23:53:39 -0300

    Entidades endereco e cliente

---
+++ src/main/java/com/HudLuca/TestTM/domain/Cliente.java
@@ -0,0 +1,118 @@
+package com.HudLuca.TestTM.domain;
+
+import com.HudLuca.TestTM.domain.enums.TipoCliente;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.*;
+
+@Entity
+@Table(name = "tb_cliente")
+public class Cliente implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nome;
+    @Column(unique = true)
+    private String email;
+    private String cpfOuCnpj;
+    private Integer tipoCliente;
+
+    @OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL)
+    private List<Endereco> enderecos = new ArrayList<>();
+
+    @ElementCollection
+    @CollectionTable(name = "tb_telefone")
+    private Set<String> telefones = new HashSet<>();
+
+    public Cliente() {
+    }
+
+    public Cliente(String nome, String email, String cpfOuCnpj, TipoCliente tipoCliente) {
+        this.nome = nome;
+        this.email = email;
+        this.cpfOuCnpj = cpfOuCnpj;
+        this.tipoCliente = (tipoCliente == null) ? null : tipoCliente.getCd();
+    }
+
+    public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipoCliente) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.cpfOuCnpj = cpfOuCnpj;
+        this.tipoCliente = (tipoCliente == null) ? null :  tipoCliente.getCd();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCpfOuCnpj() {
+        return cpfOuCnpj;
+    }
+
+    public void setCpfOuCnpj(String cpfOuCnpj) {
+        this.cpfOuCnpj = cpfOuCnpj;
+    }
+
+    public TipoCliente getTipoCliente() {
+        return TipoCliente.toEnum(this.tipoCliente);
+    }
+
+    public void setTipoCliente(TipoCliente tipoCliente) {
+        this.tipoCliente = tipoCliente.getCd();
+    }
+
+    public List<Endereco> getEnderecos() {
+        return enderecos;
+    }
+
+    public void setEnderecos(List<Endereco> enderecos) {
+        this.enderecos = enderecos;
+    }
+
+    public Set<String> getTelefones() {
+        return telefones;
+    }
+
+    public void setTelefones(Set<String> telefones) {
+        this.telefones = telefones;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Cliente cliente = (Cliente) o;
+
+        return Objects.equals(id, cliente.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+}
