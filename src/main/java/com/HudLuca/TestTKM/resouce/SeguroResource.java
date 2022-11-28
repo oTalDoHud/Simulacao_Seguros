a2d83e7bdacb03991d9f8731e039a0a1d241ae6a
oTalDoHud <hudson.lucas.vieira@gmail.com>
2022-11-27 23:53:01 -0300

    Alteração dos endpointes para o plural

--- src/main/java/com/HudLuca/TestTKM/resouce/SeguroResource.java
+++ src/main/java/com/HudLuca/TestTKM/resouce/SeguroResource.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/seguro")
+@RequestMapping(value = "/seguros")
 public class SeguroResource {
 
     @Autowired
